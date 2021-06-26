e9ef0e4be9341947d3140da096b490a7eeb80616
marcelpinotti <marcel.pinotti@gmail.com>
2021-06-26 15:43:32 -0300

    rodando o primeiro teste

---
+++ src/test/java/com/teste/CalculadoraTest.java
@@ -0,0 +1,19 @@
+package com.teste;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void somar() {
+
+        Calculadora calc = new Calculadora();
+
+        int soma = calc.somar ("1+1+3");
+        assertEquals(5, soma);
+
+
+    }
+}
